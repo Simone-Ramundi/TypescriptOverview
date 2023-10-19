1338ccc2dd84c06425d5e1756d641e9068e94b33
Simone Ramundi <simone.ramundi@4science.com>
2023-10-19 17:04:35 +0200

    TS with function

--- ts/index.ts
+++ ts/index.ts
@@ -6,3 +6,9 @@ const myName: string = "Simone";
 /* .include METHOD */
 const sentence = 'This is a sentence';
 sentence.includes('is');
+
+/* TS with Functions */
+const add = (a:number, b:number):number =>{
+    return a + b;
+}
+
