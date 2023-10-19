686e4e88b8ded162af2f3f100973d1969d7d3c75
Simone Ramundi <simone.ramundi@4science.com>
2023-10-19 16:21:28 +0200

    .include METHOD

--- ts/index.ts
+++ ts/index.ts
@@ -1,9 +1,8 @@
 console.log('Hi there!');
 
 /* CONST DEFINES A STRING */
-
 const myName: string = "Simone";
 
-/* */
-
-
+/* .include METHOD */
+const sentence = 'This is a sentence';
+sentence.includes('is');
