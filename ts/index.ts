1173e10e666370d710c4bf82e8461c92d6e7642e
Simone Ramundi <simone.ramundi@4science.com>
2023-10-24 11:03:01 +0200

    Classes and Properties

--- ts/index.ts
+++ ts/index.ts
@@ -28,3 +28,27 @@ interface PostGatekeeper {
 const printPrint = (postToPrint: PostGatekeeper) => {
     return  `${postToPrint.title} (${postToPrint.daysOld} days old`
 }
+
+/* Classes and Properties */
+
+class Car {
+    /* EVERY CAR SHOULD HAVE COLOR AND YEAR: */
+    /* PROPERTIES :*/
+    color: string;
+    year: number;
+     /* CONSTRUCTOR FX: */
+    constructor( color: string, year: number ) {
+        /* QUI VENGONO INIZIALIZZATE */
+        this.color = color;
+        this.year = year;
+    }
+    drive() {
+        console.log('Vroom')
+    }
+}
+/* COMMAND TO CREATE AN INSTACE OF CAR*/
+const myCar = new Car('red', 2000);
+/* COMMAND TO CALL A METHOD */
+myCar.drive();
+/* CONSOLE LOG OF THE NEW PROPERTIES: */
+console.log(myCar.color, myCar.year); // 'red' ,2000
